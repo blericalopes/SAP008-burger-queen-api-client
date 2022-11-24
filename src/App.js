d331bcb18c989218e7b50c4d347c4ac45c1ae66c
blericalopes <blericalopes@outlook.com>
2022-11-23 23:00:39 -0300

    acessando por tipo a partir do click, array de strings no console

--- src/App.js
+++ src/App.js
@@ -3,14 +3,11 @@ import { Routes, Route } from "react-router-dom"
 import { Login } from "./pages/Login" 
 import { Signin } from "./pages/Signin"
 import { System } from "./pages/System"
-import { Products } from "./pages/Products"
-
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
-      <Route path="/products" element={<Products />} />
       <Route path="/signin" element={<Signin />} />
       <Route path="/system" element={<System />} />
     </Routes>
