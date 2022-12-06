5b5fa39694815ceb63e4fc48025fc6ef02141406
blericalopes <blericalopes@outlook.com>
2022-12-05 21:28:39 -0300

    retornando o obj todo dos itens que tem type breakfast

--- src/pages/System.js
+++ src/pages/System.js
@@ -1,44 +1,63 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-/* eslint-disable react-hooks/rules-of-hooks */
 import { useEffect, useState } from "react"
 import { myMenu } from "../api"
 import Button from "../components/button"
 import Menu from "../components/menu"
 
 export function System() {
-
   const [products, setProducts] = useState([])
-
 
   useEffect(() => {
     myMenu()
-      .then((products) => setProducts(products))
+    .then((products) => setProducts(products))
   }, [])
 
-  const breakfast = () => {
-    const menu = products.map((prod) => prod.type)
-    console.log(menu.filter(itens => itens.includes("breakfast")))
-    
-  }
+  const menu = 
+    products.filter((prod) => {
+      let breakfast = '';
+      if(prod.type.includes('breakfast') === true) {
+        breakfast = prod.name
+        return breakfast
+      }
+    })  
+  
 
-  const allDay = () => {
-    const menu = products.map((prod) => prod.type)
-    console.log(menu.filter(itens => itens.includes("all-day")))
-  }
+  
+  
 
+  // const menu = products.map((prod) => prod.type)
+  // console.log(menu.filter(itens => itens.includes("breakfast")))
 
   return (
     <>
       <div className="system-buttons">
-        <Button onClick={breakfast} text={"Café da manhã"} />
-        <Button onClick={allDay} text={'Cardápio dia todo'} />
+        <Button onClick={console.log(menu)}text={"Café da manhã"} />
+        <Button text={'Cardápio dia todo'} />
       </div>
       <div className="system-buttons-orders">
         <Button text={"Pedidos prontos"} />
       </div>
       <div className="first-menu">
-        <Menu text={"itens itens itens itens"}></Menu>
+        <Menu></Menu>
       </div>      
     </>
   )
 }
+
+
+  //  const breakfast = () => {
+    //  const menu = products.map((prod) => prod.type)
+    // console.log(menu.filter(itens => itens.includes("breakfast")))
+      
+  //  }
+  
+  //  const allDay = () => {
+  //    const menu = products.map((prod) => prod.type)
+  //    console.log(menu.filter(itens => itens.includes("all-day")))
+  //  }
+
+  // `<div class=breakfast-container>
+        // <p>${prod.name}</p>
+        //  <p>${prod.flavor}</p>
+        // <p>${prod.complement}</p>
+        // <p>${prod.proce}</p>
+        // </div>`
