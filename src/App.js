9747fa39e64861ec48615a9f3583d77a94efe7d7
blericalopes <blericalopes@outlook.com>
2022-12-08 19:50:21 -0300

    acesso ao sistema em uma página só

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Routes, Route } from "react-router-dom"
 import { Login } from "./pages/Login" 
 import { Signin } from "./pages/Signin"
 import { System } from "./pages/System"
-import { MenuBreakfast } from "./pages/MenuBreakfast"
 
 function App() {
   return (
@@ -11,7 +10,6 @@ function App() {
       <Route path="/" element={<Login />} />
       <Route path="/signin" element={<Signin />} />
       <Route path="/system" element={<System />} />
-      <Route path="/menu_breakfast" element={<MenuBreakfast />} />
     </Routes>
   );
 }
