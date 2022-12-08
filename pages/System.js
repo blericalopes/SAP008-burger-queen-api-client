601a99c92398c2765e90c9c05a855c5e5b851f03
blericalopes <blericalopes@outlook.com>
2022-12-08 10:51:05 -0300

    acessando os dois menus de forma separada

--- pages/System.js
+++ pages/System.js
@@ -1,24 +1,38 @@
 import Button from "../components/button"
-import { Menu } from "./Menu"
+import { MenuBreakfast } from "./MenuBreakfast"
+import { MenuAllDay } from "./MenuAllDay"
 
 export function System() {
 
+  const firstMenu = document.querySelector('#first-menu')
+  const secondMenu = document.querySelector('#second-menu')
+
+  const seeMenuBreakfast = () => {
+    return firstMenu.style.display = 'flex'
+  }
+  const closeMenuBreakfast = () => {
+    firstMenu.style.display = 'none'
+  }
+  const seeMenuAllDay = () => {
+    return secondMenu.style.display = 'flex'
+  }
+  const closeMenuAllDay = () => {
+    secondMenu.style.display = 'none'
+  }
+
   return (
-    <>
+    <div>
       <div className="system-buttons">
-        <Button text={"Café da manhã"} />
-        <Button text={'Cardápio dia todo'} />
-      </div>
-      <div className="system-buttons-orders">
+        <Button onClick={seeMenuBreakfast} text={"Café da manhã"} />
+        <Button onClick={seeMenuAllDay} text={'Cardápio dia todo'} />
         <Button text={"Pedidos prontos"} />
       </div>
       <div id="first-menu">
-        <span>{Menu()}</span>
+        <span className="breakfast-list">{MenuBreakfast()} <button className="close" onClick={closeMenuBreakfast}>Fechar</button></span>
       </div>
-
-    </>
-
+      <div id="second-menu">
+        <span className="breakfast-list">{MenuAllDay()} <button className="close all" onClick={closeMenuAllDay}>Fechar</button></span>
+      </div>
+    </div>
   )
-  
-
 }
